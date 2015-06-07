841dddf606918634816ddb8988ecfcb154fce2c4
sneik- <risto.kaartinen@gmail.com>
2015-06-07 10:58:24 +0300

    Prototyyppi valmis: sidebarmenuun lisää teemoja ja luo muutamaan kohtaan visualisointeja.

--- server.R
+++ server.R
@@ -1,7 +1,28 @@
 library(shiny)
+library(rCharts)
+library(dygraphs)
+library(dplyr)
+library(DT)
+lungDeaths <- cbind(mdeaths, fdeaths)
 
 shinyServer(
     function(input,output) {
+        output$aikasarja <- renderDygraph({
+            cbind(mdeaths, fdeaths) %>% 
+            dygraph() %>% 
+            dyRangeSelector()
+        })
         
+        output$iris <- DT::renderDataTable(iris, filter = 'top', options = list(lengthChange = FALSE))
+        
+        output$myChart <- renderChart2({
+             r1 <- rPlot(mpg ~ wt | am + vs, data = mtcars, type = "point", color = "gear")
+         })
+        
+        output$myChart2 <- renderChart2({
+            as.data.frame(HairEyeColor) %>% 
+            filter(Sex == "Male") %>%
+            nPlot(Freq ~ Hair, group = "Eye", data = ., type = "multiBarHorizontalChart")
+        })    
     }
 )
--- ui.R
+++ ui.R
@@ -1,22 +1,83 @@
 library(shiny)
 library(shinydashboard)
+library(dygraphs)
+library(rCharts)
 
 ui <- dashboardPage(
     dashboardHeader(title = "Datavisualisaattori"),
     dashboardSidebar(
-        menuItem("Sairauspoissaolot", 
+        sidebarMenu(
+            menuItem("Sairauspoissaolot", 
                  tabName = "sairauspoissaolot", 
-                 icon = icon("heart")),
-        menuItem("Työkyvyttömyyseläkkeet", 
+                 icon = icon("heart-o")),
+            menuItem("Työkyvyttömyyseläkkeet", 
                  tabName = "tkElake", 
                  icon = icon("wheelchair")),
-        menuItem("Työtapaturmat", 
+            menuItem("Työtapaturmat", 
                  tabName = "tapaturmat", 
-                 icon = icon("frow-o")),
-        menuItem("Työterveyshuolto", 
+                 icon = icon("flash")),
+            menuItem("Työterveyshuolto", 
                  tabName = "tth", 
-                 icon = icon("stetoscope"))
+                 icon = icon("stethoscope")),
+            menuItem("Vakuutetut", 
+                     tabName = "vakuutetut", 
+                     icon = icon("user")),
+            menuItem("Eläkeratkaisutoiminta", 
+                     tabName = "ratkaisutoiminta", 
+                     icon = icon("bar-chart")),
+            menuItem("Tutkimukset", 
+                     tabName = "tutkimukset", 
+                     icon = icon("graduation-cap")),
+            menuItem("Asiakastyytyväisyys ", 
+                     tabName = "asty", 
+                     icon = icon("smile-o")),
+            menuItem("Rahoitus", 
+                     tabName = "rahoitus", 
+                     icon = icon("euro")),
+            menuItem("Ostolaskut", 
+                     tabName = "ostolaskut", 
+                     icon = icon("credit-card")),
+            menuItem("Keva organisaationa", 
+                     tabName = "keva", 
+                     icon = icon("institution"))
+        )
     ),
-    dashboardBody("Tähän kuvio")
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "sairauspoissaolot",
+                    fluidPage("Aikasarja",dygraphOutput("aikasarja"))
+                    ),
+             tabItem(tabName = "tkElake", 
+                     fluidPage("Ristiintaulukoitu",showOutput("myChart", "polycharts"))
+              ),
+             tabItem(tabName = "tapaturmat",
+                     fluidPage(showOutput("myChart2", "nvd3"))
+             ),
+            tabItem(tabName = "tth",
+                    tags$iframe(src = "http://rpubs.com/welxo88/kela_tth_IvsIIsuhde", seamless=NA, width=1200, height=800)
+            ),
+            tabItem(tabName = "vakuutetut",
+                    fluidPage("Taulukko", DT::dataTableOutput("iris"))
+            ),
+            tabItem(tabName = "ratkaisutoiminta",
+                    "Tähän eläkeratkaisutilastoja"
+                    ),
+            tabItem(tabName = "tutkimukset",
+                    "Tähän tutkimusvisualisointeja"
+            ),
+            tabItem(tabName = "asty",
+                    "Tähän asty-visualisointeja"
+            ),
+            tabItem(tabName = "rahoitus",
+                    "Tähän rahoitusvisualisointeja"
+            ),
+            tabItem(tabName = "ostolaskut",
+                    "Tähän ostolaskuvisualisointeja"
+            ),
+            tabItem(tabName = "keva",
+                    "Tieto kevasta"
+            )
+        )
+    )
 )
     
